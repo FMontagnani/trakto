d8a22da7d9cc7c2897045ef1717d85167e959294
FMontagnani <fabricio_montagnani@hotmail.com>
2025-05-22 18:42:32 -0300

    chore: updated deps, eslint and docker file

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,21 @@
-FROM node:14
+FROM node:20-alpine
 
 # Set the working directory
 WORKDIR /usr/src/app
 
-# Copy package.json and package-lock.json
+# Copy package files
 COPY package*.json ./
 
-# Install dependencies
+# Install dependencies - including dev dependencies for building
 RUN npm install
 
 # Copy the rest of the application code
 COPY . .
 
-# Build the TypeScript code
+# Build the TypeScript code 
 RUN npm run build
 
 # Expose the application port
 EXPOSE 3000
 
-# Command to run the application
 CMD ["node", "dist/adapters/api/app.js"]
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,17 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.strict,
-  tseslint.configs.stylistic
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  {
+    ignores: ['dist/'],
+    plugins: {
+      '@stylistic': stylistic
+    },
+    rules: {
+      '@stylistic/quotes': ['error', 'single']
+    }
+  }
 );
